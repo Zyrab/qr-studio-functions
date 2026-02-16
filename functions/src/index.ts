0ffa639f697e0547687028bf8ce963a8a5c369f8
zyrab <zurakrua@gmail.com>
2026-02-16 17:06:40 +0100

    ref: formated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,10 +5,7 @@ import { setGlobalOptions } from "firebase-functions/v2";
  * - region: Updated to 'europe-west3' to match your Firestore database location.
  * - maxInstances: Set to 10 to control costs and prevent over-scaling.
  */
-setGlobalOptions({ 
-  region: "europe-west3", 
-  maxInstances: 10 
-});
+setGlobalOptions({region: "europe-west3", maxInstances: 10 });
 
 // Import the trial handler
 import { handleTrialStart } from "./handlers/trials";
@@ -19,4 +16,4 @@ import { createUser } from "./handlers/create-user";
 import { createQRCode } from "./handlers/create-qr-code";
 import { redirectQR } from "./handlers/redirect-qr-code";
 // Export the functions
-export { handleTrialStart, createCheckoutSession, getCustomerPortalLink, stripeWebhook, createUser, createQRCode,redirectQR };
+export { handleTrialStart, createCheckoutSession, getCustomerPortalLink, stripeWebhook, createUser, createQRCode, redirectQR };
